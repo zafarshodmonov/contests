ff3dc56ee42d6ff4b97d68aaa022da4fffa7d448
zafarshodmonov1996 <zafarshodmonov1996@gmail.com>
2024-11-09 23:56:39 +0500

    z

---
+++ contest_11_09_2024_id_202940/solutions/solution_B/main.py
@@ -0,0 +1,3 @@
+# Directions for moving up, down, left, and right
+directions_list = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+directions_dict = {'u': (-1, 0), 'd': (1, 0), 'l': (0, -1), 'r': (0, 1)}
--- contest_11_09_2024_id_202940/solutions/solution_F/main.py
+++ contest_11_09_2024_id_202940/solutions/solution_F/main.py
@@ -1,46 +1,39 @@
 
 def input_data():
     N, M = map(int, input().split())
-    nums = [[1 if j == "." else 0 for j in i] for i in input()]
+    nums = [[1 if j == "." else 0 for j in input()] for i in range(N)]
     return N, M, nums
 
 N, M, nums = input_data()
 pos = [(0, -1), (-1, 0), (0, 1), (1, 0)]
 
 def processing(N: int, M: int, nums: list[list[int]]) -> int:
-    vis = set()
-    
-
+    vis = [[False] * M for _ in range(N)]
     def dfs(i, j):
-        if (i, j) in vis:
+        if vis[i][j]:
             pass
         else:
-            vis.add((i, j))
-            pass
+            vis[i][j] = True
             for p in pos:
                 ni = p[0] + i
                 nj = p[1] + j
                 if 0 <= ni < N and 0 <= nj < M:
-                    dfs(ni, nj)
-                    pass
-                else:
-                    pass
-                pass
+                    if nums[ni][nj] == 1:
+                        dfs(ni, nj)
+                    else:
+                        vis[ni][nj] = True
     sana = 0
     for i in range(N):
         for j in range(M):
-            if (i, j) in vis:
+            if vis[i][j]:
                 pass
             else:
                 if nums[i][j] == 1:
-                    vis.add((i, j))
+                    sana += 1
                     dfs(i, j)
-                    pass
                 else:
-                    vis.add((i, j))
-                    pass
-                pass
-    pass
+                    vis[i][j] = True
+    return sana
 
 ans = processing(N, M, nums)
 print(ans)
