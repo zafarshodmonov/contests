67f063419a9248070f01fcbf7a0258a3a33014b1
zafarshodmonov1996 <zafarshodmonov1996@gmail.com>
2024-11-15 22:47:33 +0500

    a

--- contest_11_09_2024_id_202940/solutions/solution_B/B_idea1.py
+++ contest_11_09_2024_id_202940/solutions/solution_B/B_idea1.py
@@ -1,68 +1,111 @@
 from collections import deque
 
-def new_coor(old: tuple, direction: str | tuple):
-    if type(direction) is str:
-        direction = {'u': (-1, 0), 'd': (1, 0), 'l': (0, -1), 'r': (0, 1)}[direction]
-    else:
-        pass
-    return old[0] + direction[0], old[1] + direction[1]
 
 class Barrier:
 
     def __init__(self):
-        self.__B = [[False] * 8] * 8
+        self.__B = [[False for j in range(8)] for i in range(8)]
     
-    def get_bar(self, y, x):
-        i = y - 1
-        j = x - 1
+    def get_bars(self):
+        return self.__B
+    
+    def get_bar(self, coor):
+        i = coor[0] - 1
+        j = coor[1] - 1
         return self.__B[i][j]
     
-    def set_bar(self, y, x, val):
-        i = y - 1
-        j = x - 1
+    def set_bar(self, coor, val):
+        i = coor[0] - 1
+        j = coor[1] - 1
         self.__B[i][j] = val
+
 
 class Snake(Barrier):
 
     def __init__(self, coor: tuple):
         super().__init__()
         self.head = coor
-        self.set_bar(1, 1, True)
-        self.tail = coor
-        self.body = coor
+        self.set_bar((1, 1), True)
+        #self.tail = coor
+        #self.body = coor
         self.snake = deque([coor])
 
+    def new_coor(self, old: tuple, direction: str | tuple):
+        if type(direction) is str:
+            direction = {'u': (-1, 0), 'd': (1, 0), 'l': (0, -1), 'r': (0, 1)}[direction]
+        else:
+            pass
+        return old[0] + direction[0], old[1] + direction[1]
 
     def run(self, direction: str, apple: bool = False):
+        new = self.new_coor(self.head, direction)
         if apple:
-            self.head = new_coor(self.head, direction)
-            self.set_bar(*self.head, True)
-            self.snake.append(self.head)
-            #self.snake.popleft()
+            if self.is_pos_coor(new):
+                self.head = new
+                self.set_bar(self.head, True)
+                self.snake.append(self.head)
+                #self.snake.popleft()
         else:
-            self.head = new_coor(self.head, direction)
-            self.set_bar(*self.head, True)
-            self.snake.append(self.head)
-            self.set_bar(*self.snake.popleft(), False)
+            if self.is_pos_coor(new):
+                self.head = new
+                self.set_bar(self.head, True)
+                self.snake.append(self.head)
+                self.set_bar(self.snake.popleft(), False)
             
 
-def is_pos_coor(y, x):
-    if 1 <= y <= 8 and 1 <= x <= 8:
-        return True
+    def is_pos_coor(self, coor):
+        y = coor[0]
+        x = coor[1]
+        if 1 <= y <= 8 and 1 <= x <= 8:
+            return True
+        else:
+            return False
+
+
+def bfs(start_point: tuple, end_point: tuple, snake: Snake) -> int:
+    queue = deque([(start_point, 0)])  # (current_position, distance)
+    visited = set([start_point])
+    directions = ['u', 'd', 'l', 'r']
+
+    while queue:
+        current, dist = queue.popleft()
+        
+        if current == end_point:
+            #snake.run(current, True)
+            return dist
+        
+        for direction in directions:
+            next_pos = snake.new_coor(current, direction)
+            
+            if snake.is_pos_coor(next_pos) and not snake.get_bar(next_pos) and next_pos not in visited:
+                apple = True if next_pos == end_point else False
+                snake.run(direction, apple)
+                #print("snake: ", snake.snake)
+                visited.add(next_pos)
+                queue.append((next_pos, dist + 1))
+    
+    return -1  # Agar yo'l topilmasa
+
+def inp():
+    rel = []
+    n = int(input())
+    for i in range(n):
+        y, x = map(int, input().split())
+        rel.append((y, x))
+    return rel
+
+sana = 0
+pos_cor = inp()
+snake = Snake((1, 1))
+dast = (1, 1)
+for p in pos_cor:
+    m = bfs(dast, p, snake)
+    dast = p
+    #print("m: ", m)
+    if m > 0:
+        sana += m
     else:
-        return False
-
-
-
-def f(start_point: tuple, end_point: tuple, snake: Snake) -> int:
-    pass
-
-B = Barrier()
-B.set_bar(1, 1, True)
-
-y = int(input("y: "))
-x = int(input("x: "))
-
-# if is_pos_coor(y, x):
-#     print(B.get_bar(y, x))
-#     print('New coor: ', new_coor((y, x), input("tomon: ")))
+        pass
+print(snake.get_bars())
+print(sana)
+print(snake.head)
---
+++ contest_11_09_2024_id_202940/solutions/solution_B/B_idea2.py
@@ -0,0 +1,97 @@
+from itertools import permutations
+from collections import deque
+
+# Yangi koordinata qaytarish funksiyasi
+def new_coor(old: tuple, direction: str | tuple):
+    if type(direction) is str:
+        direction = {'u': (-1, 0), 'd': (1, 0), 'l': (0, -1), 'r': (0, 1)}[direction]
+    return old[0] + direction[0], old[1] + direction[1]
+
+# 8x8 doskada harakat cheklovlari uchun yordamchi funksiya
+def is_pos_coor(y, x):
+    return 1 <= y <= 8 and 1 <= x <= 8
+
+# Barrier klassi: To'siqlarni saqlash uchun
+class Barrier:
+    def __init__(self):
+        self.__B = [[False] * 8 for _ in range(8)]
+    
+    def get_bar(self, y, x):
+        return self.__B[y-1][x-1]
+    
+    def set_bar(self, y, x, val):
+        self.__B[y-1][x-1] = val
+
+# Ilon klassi: Ilonning harakati va tanasi uchun
+class Snake(Barrier):
+    def __init__(self, coor: tuple):
+        super().__init__()
+        self.head = coor
+        self.set_bar(coor[0], coor[1], True)
+        self.snake = deque([coor])
+
+    def run(self, direction: str, apple: bool = False):
+        new_head = new_coor(self.head, direction)
+        
+        if apple:
+            self.head = new_head
+            self.set_bar(*self.head, True)
+            self.snake.append(self.head)
+        else:
+            self.head = new_head
+            self.set_bar(*self.head, True)
+            self.snake.append(self.head)
+            tail = self.snake.popleft()
+            self.set_bar(*tail, False)
+
+# BFS yordamida ikkita nuqta orasidagi eng qisqa masofani hisoblash
+def bfs(start, end, snake):
+    queue = deque([(start, 0)])  # (koordinata, masofa)
+    visited = set([start])
+    directions = ['u', 'd', 'l', 'r']
+    
+    while queue:
+        current, dist = queue.popleft()
+        
+        if current == end:
+            return dist
+        
+        for direction in directions:
+            next_pos = new_coor(current, direction)
+            y, x = next_pos
+            
+            if is_pos_coor(y, x) and not snake.get_bar(y, x) and next_pos not in visited:
+                visited.add(next_pos)
+                queue.append((next_pos, dist + 1))
+    
+    return float('inf')  # Yo'l topilmasa
+
+# Minimal yo'lni topish uchun asosiy funksiya
+def minimal_snake_path(start, apples):
+    min_path = float('inf')
+    
+    for order in permutations(apples):
+        snake = Snake(start)  # Har bir tartib uchun yangi ilon yaratiladi
+        total_dist = 0
+        current_position = start
+        
+        for apple in order:
+            dist = bfs(current_position, apple, snake)
+            if dist == float('inf'):  # Agar yo'l topilmasa
+                total_dist = float('inf')
+                break
+            total_dist += dist
+            snake.run('r', apple=True)  # Ilonni olma joylashgan nuqtaga olib boramiz
+            current_position = apple
+        
+        min_path = min(min_path, total_dist)
+    
+    return min_path
+
+# Kirish qismi
+N = int(input().strip())
+apples = [tuple(map(int, input().split())) for _ in range(N)]
+
+# Chiqish qismi
+start = (1, 1)
+print(minimal_snake_path(start, apples))
